f4774e182244b7fd8128a7684346ca237307ac84
conner <nocergela@pm.me>
2026-06-02 21:09:06 -0400

    Remove unused test helpers

--- Tests/WaylandRawTests/Shims/ShimRequestRecordingLock.swift
+++ Tests/WaylandRawTests/Shims/ShimRequestRecordingLock.swift
@@ -2,7 +2,6 @@ import Synchronization
 
 enum ShimRequestRecordingLock {
     static let activation = Mutex<Void>(())
-    static let data = Mutex<Void>(())
     static let pointerCapture = Mutex<Void>(())
     static let primarySelection = Mutex<Void>(())
     static let scale = Mutex<Void>(())
