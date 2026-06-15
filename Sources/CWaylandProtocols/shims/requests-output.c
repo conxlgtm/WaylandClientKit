8869cda47eaa5e204ab65d0c09d6161a936b536e
conxlgtm <83147518+conxlgtm@users.noreply.github.com>
2026-06-15 02:57:55 -0400

    Fix protocol lifetime review issues

--- Sources/CWaylandProtocols/shims/requests-output.c
+++ Sources/CWaylandProtocols/shims/requests-output.c
@@ -2,6 +2,23 @@
 #include "generated/legacy-unstable/xdg-output/xdg-output-unstable-v1-client-protocol.h"
 #include "generated/wlr-unstable/output-management/wlr-output-management-unstable-v1-client-protocol.h"
 
+#ifdef SWL_ENABLE_TESTING
+static int swl_test_output_request_recording_enabled;
+static struct swl_test_output_destroy_record swl_test_output_destroy_latest;
+
+static void swl_test_record_output_destroy(
+    enum swl_test_output_destroy_kind kind,
+    void *object)
+{
+    if (!swl_test_output_request_recording_enabled)
+        return;
+
+    swl_test_output_destroy_latest.call_count += 1;
+    swl_test_output_destroy_latest.kind = kind;
+    swl_test_output_destroy_latest.object = object;
+}
+#endif
+
 struct zxdg_output_v1 *swl_zxdg_output_manager_v1_get_xdg_output(
     struct zxdg_output_manager_v1 *manager,
     struct wl_output *output)
@@ -43,9 +60,31 @@ void swl_zwlr_output_head_v1_destroy(struct zwlr_output_head_v1 *head)
     zwlr_output_head_v1_destroy(head);
 }
 
+void swl_zwlr_output_head_v1_release(struct zwlr_output_head_v1 *head)
+{
+#ifdef SWL_ENABLE_TESTING
+    if (swl_test_output_request_recording_enabled) {
+        swl_test_record_output_destroy(SWL_TEST_OUTPUT_HEAD_RELEASE, head);
+        return;
+    }
+#endif
+    zwlr_output_head_v1_release(head);
+}
+
 void swl_zwlr_output_mode_v1_destroy(struct zwlr_output_mode_v1 *mode)
 {
     zwlr_output_mode_v1_destroy(mode);
+}
+
+void swl_zwlr_output_mode_v1_release(struct zwlr_output_mode_v1 *mode)
+{
+#ifdef SWL_ENABLE_TESTING
+    if (swl_test_output_request_recording_enabled) {
+        swl_test_record_output_destroy(SWL_TEST_OUTPUT_MODE_RELEASE, mode);
+        return;
+    }
+#endif
+    zwlr_output_mode_v1_release(mode);
 }
 
 struct zwlr_output_configuration_head_v1 *
@@ -124,3 +163,24 @@ void swl_zwlr_output_configuration_head_v1_destroy(
 {
     zwlr_output_configuration_head_v1_destroy(head);
 }
+
+#ifdef SWL_ENABLE_TESTING
+void swl_test_output_request_recording_begin(void)
+{
+    swl_test_output_request_recording_enabled = 1;
+    swl_test_output_destroy_latest =
+        (struct swl_test_output_destroy_record){0};
+}
+
+void swl_test_output_request_recording_end(void)
+{
+    swl_test_output_request_recording_enabled = 0;
+    swl_test_output_destroy_latest =
+        (struct swl_test_output_destroy_record){0};
+}
+
+struct swl_test_output_destroy_record swl_test_output_destroy_record(void)
+{
+    return swl_test_output_destroy_latest;
+}
+#endif
